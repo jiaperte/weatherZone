38347f76b0905bbee4bb19a9905acbd0ffa5a054
jiayong <jiayong_2010@139.com>
2019-12-15 21:08:44 +1100

    add weatherZone directory

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /home/ec2-user/weatherZone-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ec2-user/weatherZone/weatherZone-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/weatherZone/start_server.sh
+chmod +x /home/ec2-user/weatherZone/stop_server.sh
 
